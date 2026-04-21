378aad4f7f096a2c1add9eee47ff0c7ace420d65
iaidrew <adityaofficial9918@gmail.com>
2026-04-21 19:11:05 +0530

    fix: moved logo to public folder

--- src/App.tsx
+++ src/App.tsx
@@ -58,7 +58,7 @@ const staggerContainer = {
 // --- Shared Components ---
 
 const Logo = () => (
-  <div className="flex items-center group cursor-pointer h-15">
+  <div className="flex items-center group cursor-pointer h-14">
     <img 
       src="/assets/opsiyslogo.png" 
       alt="OPSIYS" 
